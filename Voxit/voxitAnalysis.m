0d708d37b8049c05c1d7e9ed55b350eaa043b0bd
Lee M. Miller <leemiller@ucdavis.edu>
2021-09-28 07:55:56 -0700

    minor edit to clarify % comments

--- Voxit/voxitAnalysis.m
+++ Voxit/voxitAnalysis.m
@@ -349,8 +349,8 @@ Iaccel = [];
 for i = 1:size(ixvoicedbounds,1)  %the loop makes sure we're not calculating velocity etc across segments
     difflogItmp = difflogI(ixvoicedbounds(i,1):ixvoicedbounds(i,2));    
     IsegmentMeans = [IsegmentMeans; mean(difflogItmp)];
-    Ivelocity = [Ivelocity; diff(difflogItmp)/ts]; %norm by sampling period, so in octaves per sec
-    Iaccel = [Iaccel; diff(diff(difflogItmp))/ts]; %in octaves per sec per sec
+    Ivelocity = [Ivelocity; diff(difflogItmp)/ts]; %norm by sampling period, so in log power per sec
+    Iaccel = [Iaccel; diff(diff(difflogItmp))/ts]; %in log power per sec per sec
 end
 
 S.analysis.Intensity = logPower;
@@ -438,7 +438,8 @@ S.analysis.Corrf0IntensityPvalue = corrf0Ipval(1,2);
 
 %%  COMPOSITE METRICS
 a=S.analysis;
-S.analysis.dynamism = (abs(a.f0speed) * a.f0entropy) + (a.complexitySyllables+a.complexityPhrases)/2*0.439; % 43.9 brings it into same range as first term, used for the final 100poets analysis.  1/2*0.439 = 0.2195
+%S.analysis.dynamism = (abs(a.f0speed) * a.f0entropy) + (a.complexitySyllables+a.complexityPhrases)/2*0.439; % 43.9 brings it into same range as first term, used for the final 100poets analysis.  1/2*0.439 = 0.2195
+S.analysis.Dynamism = (abs(a.f0MeanAbsVelocity) * a.f0Entropy) + (a.ComplexitySyllables+a.ComplexityPhrases)/2*0.439; % updated variable names from original formula, still, 43.9 brings it into same range as first term, used for the final 100poets analysis.  1/2*0.439 = 0.2195
 %S.analysis.Dynamism = abs(a.f0MeanAbsVelocity)/1.092050992 + a.f0Entropy/3.331034878 + (a.ComplexitySyllables/13.18735087+a.ComplexityPhrases/3.022951534)/2; % bring terms into same range ~1
 %S.analysis.Dynamism = (a.f0MeanAbsVelocity/.1167627388 + a.f0Entropy/.3331034878)/2 + a.ComplexityAllPauses/.6691896835; % normalization factors from 100 poets, so each term ~10
 
